d386b1d84b0468a91720f5158bc9f7f6ffc869db
tomato <ljc6qx@163.com>
2017-02-25 23:57:56 +0800

    update package.json

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const {json} = require('micro');
+const fetch = require('node-fetch')
 
-module.exports = async req => {
-	const body = await json(req);
+module.exports = async (req, res) => {
+	// const body = await json(req);
 
 	// let's echo the text
-	return {text: body.text};
+	// return {text: body.text};
+  // return 'Data logged to your console'
+
+  const response = await fetch('https://apiv2.okoer.com/v2/articles?type=news')
+  const json = await response.json()
+  console.log(json)
+  return json
 };
+
+// module.exports = (req, res) => 'Welcome to micro'
